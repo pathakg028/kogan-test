6e7f85a8f9e2a5be6b3997baffe731ab2fc03eb2
Garry <pathakg028@gmail.com>
2019-08-13 16:33:03 +1000

    added eslint and wrote pseudocode for tests

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const axios = require('axios');
+const axios = require('axios')
 
 // make the array that we'll push our responses into
-let arr = []
+const arr = []
 
 // hitting the api function
 const getData = async (nextEndpoint) => {
   // sometimes we don't get the next endpoint
-  let endpoint;
+  let endpoint
   if (nextEndpoint === undefined) {
     endpoint = '/api/products/1'
   } else {
     endpoint = nextEndpoint
   }
   const url = `http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com${endpoint}`
-  return await axios.get(url)
+  const endpt = await axios.get(url)
+  return endpt
 }
 
 const accessData = async (nextEndpoint) => {
@@ -21,7 +22,7 @@ const accessData = async (nextEndpoint) => {
     // nextEndpoint doesn't exist here
     const result = await getData()
     arr.push(result.data)
-    // accessData will now take a nextEndpoint as an argument
+    // accessData will now take a nextEndpoint as an argument, the first example of recursion
     accessData(result.data.next)
   } else {
     // here next endpoint does exist
@@ -32,18 +33,15 @@ const accessData = async (nextEndpoint) => {
       const products = getOnlyProducts(arr)
       // ensuring that we only have product objects in our array, no next strings
       const airConditioners = filterAirconditioners(products)
-      //console.log(airConditioners)
       const cubicWeight = []
-      airConditioners.forEach((item)=>{
-        cubicWeight.push(Number((item.size.width/100*item.size.length/100*item.size.height/100).toFixed(2)))
+      airConditioners.forEach((item) => {
+        cubicWeight.push(Number((item.size.width / 100 * item.size.length / 100 * item.size.height / 100).toFixed(2)))
       })
-      const cubicWeightLength = cubicWeight.length
-      //const averageCubicWeight = cubicWeight/cubicWeightLength*250
+      // const averageCubicWeight = cubicWeight/cubicWeightLength*250
 
-      const sum = cubicWeight.reduce((total, amount) => total + amount); 
-      const averageCubicWeight = sum/cubicWeight.length*250;
-      console.log(averageCubicWeight)
-
+      const sum = cubicWeight.reduce((total, amount) => total + amount)
+      const averageCubicWeight = sum / cubicWeight.length * 250
+      console.log('Average cubic weight of Air Conditioners is ' + averageCubicWeight + ' m*m*m*kg')
     } else {
       // we call access data again with the nextEndpoint as an argument
       accessData(result.data.next)
@@ -58,14 +56,14 @@ const getOnlyProducts = (arr) => {
       products.push(product)
     })
   })
-  //console.log(products)
+  // console.log(products)
   return products
 }
 
 const filterAirconditioners = (products) => {
   return products.filter((product) => {
-    //console.log(product.category)
-    return product.category === "Air Conditioners"
+    // console.log(product.category)
+    return product.category === 'Air Conditioners'
   })
 }
 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,19 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Kogan application tests', function() {
+  describe('find out the avg cubic meter kilogram for the category Air Conditioners', function() {
+    it('should return 41.375', function(){
+    });
+  });
+  describe('find out if api is hit and data is retrieved', function() {
+    it('network status should be 200', function(){
+    });
+  });
+  describe('check if string contains Air Condition only', function() {
+    it('return true if string contains Air Condition', function(){
+    });
+  });
+  describe('check if result value is only two digits after decimal ', function() {
+    it('check if two digits after decimal', function(){
     });
   });
 });
